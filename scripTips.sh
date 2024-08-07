3bcbe029d02f132f4bf752fc0a8f948f977e8287
Gabriel Ribeiro <gabriribeiro5@live.com>
2024-08-07 19:22:43 -0300

    feat(scripTips): add file

---
+++ scripTips.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Minhas referencias de Shell Script
+
+# 1. Variáveis
+
+myVar=”valor” # declaração (valida enquanto a sessão estiver aberta)
+echo $myVar   # print do valor da variável
+
+# 1.1 Variaveis de Ambiente
+
+set # exibe todas as variáveis de ambiente
+
+
+
