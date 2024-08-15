721c0159acac20519e10f1f9741ffd345242fd65
Gabriel Ribeiro <gabriribeiro5@live.com>
2024-08-15 17:53:54 -0300

    docs(3-topics): Info about Exit status, pipelines and conditions

--- scripTips.sh
+++ scripTips.sh
@@ -13,33 +13,33 @@ INFO
 
 # 1. VARIÁVEIS E ARRAYS
 
-# 1.1 VARIÁVEIS DE SISTEMA (criadas e utilizadas pelo seu script)
+#> 1.1 VARIÁVEIS DE SISTEMA (criadas e utilizadas pelo seu script)
 myVar="valor" # declaração (valida enquanto a sessão estiver aberta)
 echo $myVar   # use $ para chamar uma variável
 
 
-# 1.2 VARIÁVEIS DE AMBIENTE (
+#> 1.2 VARIÁVEIS DE AMBIENTE (
 
 set # exibe todas as variáveis de ambiente 
 env # exibe todas as variáveis de ambiente
 printenv # exibe todas as variáveis de ambiente
 
-# 1.3 MANIPULAÇÃO DE VARIÁVEIS
+#> 1.3 MANIPULAÇÃO DE VARIÁVEIS
 
 export environmentVar mySystemVar # exporta o valor da variavel de ambiente para uma variavel de sistema, para que vc possa manipular as parada com segurança
 unset $myVar # apaga a variável
 
-# 1.4 MANIPULAÇÃO DE ARRAYS
+#> 1.4 MANIPULAÇÃO DE ARRAYS
 
 newlinemyList=("item1" "item2" "3rdItem") 
 echo "myList items count: ${#mundo[0]} elements"
 
 # 2. BUSCA
 
-# 2.1 Filtro com <grep>
+#> 2.1 Filtro com <grep>
 set | grep FilterReference # <set> busca variaveis de ambiente e <grep> faz uma filtragem dos resultados que contenham FilterReference
 
-# 2.2 Buscar comandos executados recentemente
+#> 2.2 Buscar comandos executados recentemente
 fc -l RefString # retorna comandos recentes que contenham RefString
 
 fc -l First Last # retorna um range onde First é referencia para o primeiro comando desse range, assim como Last é para o ultimo comando retornado na lista
@@ -58,7 +58,7 @@ chmod +x path/to/file #executável
 man echo # exibe um manual da função echo (se estiver disponível)
 
 # 7. FUNÇÕES
-# Declaração
+#> 7.1 Declaração
 functionName() {
 	echo "Hello, World!"
 	echo "external variable value: $myvar"
@@ -66,7 +66,28 @@ functionName() {
 	echo "all paramethers $@"
 }
 
-# Chamada
+#> 7.2 Chamada
 functionName parametro1 parametro2 
+
+# 8. EXIT STATUS
+:<<'INFO'
+ status 0 (zero) = success
+ non-zero status = failure
+
+ All builtin commands use this pattern
+INFO
+
+# 9. SHELL COMMANDS
+command1 | command2 # pipe simples, o output de command1 é passado como input de command2
+
+command1 |& command2 # pipe com redirecionamento implicito de erros, faz o mesmo que um pipe simples além de assegurar que o erro de command1 também seja passado como input de command2
+
+# 10. CONDIÇÕES
+# 10.1 True or False
+test 1 = 2 ; echo $? # <test> é um validador de condições, <$?> é a variável na qual <test> salva seu output
+
+test -d parametro1 # verifica se <parametro1> é um diretorio
+
+test --help # para mais opções
 
 
